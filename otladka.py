6c5b3c206f69d3b745ebd464d51a6dde739053d2
nowhenman <tim-shirokov@yandex.ru>
2023-04-17 01:27:10 +0300

    задание 2, последнее

--- otladka.py
+++
@@ -1,4 +0,0 @@
-three_d = str(input("Your three-digit number: "))
-f_dig, s_dig, t_dig = int(three_d[0]), int(three_d[1]), int(three_d[2])
-print("The sum is: {sum}".format(sum=(f_dig + s_dig + t_dig)))
-print("The multipication is: {mul}".format(mul=(f_dig * s_dig * t_dig)))
