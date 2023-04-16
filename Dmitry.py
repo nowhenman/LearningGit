1d1b1a044cc30cad4b7ae14ce784657c184d9137
nowhenman <tim-shirokov@yandex.ru>
2023-04-16 18:40:46 +0300

    задание 13

--- Dmitry.py
+++ Dmitry.py
@@ -21,3 +21,27 @@ B = Circle
 print(id(B))
 a = B(3)
 Circle = 5
+
+
+def bin_search(massiv, num):
+    massiv.sort()
+    ind = len(massiv)//2
+    x = massiv[ind]
+    if x == num:
+        return ind
+    elif x > num:
+        massiv = massiv[:ind]
+        return bin_search(massiv, num)
+    elif x < num:
+        massiv = massiv[ind:]
+        return bin_search(massiv, num)
+    return -1
+
+
+ab = list(map(int, input("Введите список чисел: ").split()))
+needed = int(input("искомое число: "))
+print(bin_search(ab, needed))
+
+# от 0 до n-1 только перестраивать диапазон индексов, а не трогать!
+# попробуй на листе бумаги!
+# тащить границы диапазона в котором (переобозначать не массив, а его индексы!)
