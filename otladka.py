b276862adb5003c589be82a982aeb0a81932ab81
nowhenman <tim-shirokov@yandex.ru>
2023-04-16 21:21:04 +0300

    задание 10

---
+++ otladka.py
@@ -0,0 +1,3 @@
+sus = float(input("Введите число: "))
+print("больше нуля" if sus > 0 else ("равен нулю" if (sus == 0) else "меньше нуля"))
+# print("больше нуля" if sus > 0 else "меньше или равен 0")
