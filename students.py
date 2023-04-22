ad80e2f2f663c8a6a8e9063fe78e261f3031fb2d
nowhenman <tim-shirokov@yandex.ru>
2023-04-22 14:57:32 +0300

    new file student with classes

---
+++ students.py
@@ -0,0 +1,14 @@
+class Student:
+    pass
+
+
+vasya = Student()
+vasya.age = 22
+vasya.univer = 'MSU'
+
+petya = Student()
+petya.last_name = 'Smirnov'
+petya.age = 21
+print(vasya.age, vasya.univer)
+print(petya.last_name, petya.age)
+print(petya.univer)
