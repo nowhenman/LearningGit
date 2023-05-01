d3a0fe59e70d059784560dc8123526f6363bd6fb
nowhenman <tim-shirokov@yandex.ru>
2023-05-01 18:27:51 +0300

    Задание 1 -- done, Задания 2 и 3 не понял, Задание 4 -- done

---
+++ my_classes/Parent.py
@@ -0,0 +1,28 @@
+"""
+класс Parent, который содержит такие поля (члены класса): закрытые – имя родителя, фамилию, возраст,
+публичные – методы ввода данных (геттеры и сеттеры) и отображения их на экран.
+"""
+
+
+class Parent:
+    __fname = ''
+    __lname = ''
+    __age = 0
+
+    def set_fname(self, fname):
+        self.__fname = fname
+
+    def set_lname(self, lname):
+        self.__lname = lname
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_fname(self):
+        return self.__fname
+
+    def get_lname(self):
+        return self.__lname
+
+    def get_age(self):
+        return self.__age
