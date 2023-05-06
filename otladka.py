524018f491ed6a145d48a14b28c783b7e0057d0e
nowhenman <tim-shirokov@yandex.ru>
2023-05-06 23:11:08 +0300

    dunno

--- otladka.py
+++ otladka.py
@@ -1,2 +1,14 @@
+from my_classes.TwoVar import TwoVar
 
+first = TwoVar()
+
+first.set_var_a(5)
+first.set_var_b(-11)
+print(first.add_var())
+print(first.max_var())
+
+sec = TwoVar()
+
+print(sec.add_var())
+print(sec.max_var())
 
