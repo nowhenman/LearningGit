a597fc34cf63980dfa10f043a8e7d00817e2ddb5
nowhenman <tim-shirokov@yandex.ru>
2023-05-07 17:59:06 +0300

    triangle done

--- my_classes/Triangle.py
+++ my_classes/Triangle.py
@@ -6,7 +6,10 @@ import math
 
 
 class Triangle:
-    """Треугольник можно построить по трём сторонам; по стороне и прилежащим углам; по двум сторонам и углу"""
+    """
+    Треугольник можно построить по трём сторонам; по стороне и прилежащим углам; по двум сторонам и углу.
+    qa = distance from M to side a; qb = distance from M to side b; qc = distance from M to side c.
+    """
 
     def __init__(self):
         self.__a = 0
@@ -17,13 +20,23 @@ class Triangle:
         self.__ang_c = 0
         self.__perimeter = 0
         self.__square = 0
+        self.__qa = 0
+        self.__qb = 0
+        self.__qc = 0
 
-    def set_all_sides(self, c, a, b):
+    def __str__(self):
+        return f"\nA triangle with sides: {self.__a}, {self.__b} and {self.__c} and angles {self.__ang_a}," \
+               f" {self.__ang_b} and {self.__ang_c}. \n" \
+               f"The median point is {self.get_med_point()} from sides a, b and c." \
+               f"\nIts perimeter is {self.get_per()} " \
+               f"and its square is {self.get_square()}."
+
+    def set_all_sides(self, a, b, c):
         self.__c = c
         self.__a = a
         self.__b = b
-        self.__ang_a = math.acos((self.__b ** 2 + self.__c ** 2 - self.__a ** 2) / (2 * self.__b * self.__c))
-        self.__ang_b = math.acos((self.__a ** 2 + self.__c ** 2 - self.__b ** 2) / (2 * self.__a * self.__c))
+        self.__ang_a = math.degrees(math.acos((self.__b ** 2 + self.__c ** 2 - self.__a ** 2) / (2 * self.__b * self.__c)))
+        self.__ang_b = math.degrees(math.acos((self.__a ** 2 + self.__c ** 2 - self.__b ** 2) / (2 * self.__a * self.__c)))
         self.__ang_c = 180 - (self.__ang_a + self.__ang_b)
 
     def set_two_angles(self, c, ang_a, ang_b):
@@ -31,15 +44,15 @@ class Triangle:
         self.__ang_a = ang_a
         self.__ang_b = ang_b
         self.__ang_c = 180 - (self.__ang_b + self.__ang_a)
-        self.__a = self.__c * (math.sin(self.__a) / math.sin(self.__c))
-        self.__b = self.__c * (math.sin(self.__b) / math.sin(self.__c))
+        self.__a = self.__c * (math.sin(math.radians(self.__ang_a)) / math.sin(math.radians(self.__ang_c)))
+        self.__b = self.__c * (math.sin(math.radians(self.__ang_b)) / math.sin(math.radians(self.__ang_c)))
 
     def set_two_sides(self, b, a, ang_c):
         self.__b = b
         self.__a = a
         self.__ang_c = ang_c
-        self.__c = (self.__a ** 2 + self.__b ** 2 - (2 * self.__a * self.__b * math.cos(self.__ang_c))) ** 0.5
-        self.__ang_a = math.acos((self.__b ** 2 + self.__c ** 2 - self.__a ** 2) / (2 * self.__b * self.__c))
+        self.__c = (self.__a ** 2 + self.__b ** 2 - (2 * self.__a * self.__b * math.cos(math.radians(self.__ang_c)))) ** 0.5
+        self.__ang_a = math.degrees(math.acos((self.__b ** 2 + self.__c ** 2 - self.__a ** 2) / (2 * self.__b * self.__c)))
         self.__ang_b = 180 - (self.__ang_c + self.__ang_a)
 
     def get_per(self):
@@ -47,7 +60,11 @@ class Triangle:
         return self.__perimeter
 
     def get_square(self):
-        self.__square = 0.5 * (self.__a * self.__b * math.sin(self.__ang_c))
+        self.__square = 0.5 * (self.__a * self.__b * math.sin(math.radians(self.__ang_c)))
+        return self.__square
 
     def get_med_point(self):
-        pass  # todo
+        self.__qa = (self.get_square() * 2 / 3 / self.__a)
+        self.__qb = (self.get_square() * 2 / 3 / self.__b)
+        self.__qc = (self.get_square() * 2 / 3 / self.__c)
+        return [self.__qa, self.__qb, self.__qc]
---
+++ otladka2.py
@@ -0,0 +1,6 @@
+from my_classes.Triangle import Triangle
+
+a = Triangle()
+
+a.set_all_sides(3, 4, 5)
+print(a)
